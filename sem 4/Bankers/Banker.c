5bbf3d49c5416359a3215e751eeedbac30cb6d89
Akash R Chandran <chandranrakash@gmail.com>
2022-10-06 21:51:18 +0530

    updated

--- sem 4/Bankers/Banker.c
+++ sem 4/Bankers/Banker.c
@@ -10,34 +10,19 @@ int main()
 	int allocation[n][m], max[n][m], need[n][m], available[m], sequence[m];
 	printf("Enter the allocation matrix: \n");
 	for (i = 0; i < n; i++)
-	{
 		for (j = 0; j < m; j++)
-		{
 			scanf("%d", &allocation[i][j]);
-		}
-	}
 	printf("Enter the max matrix: \n");
 	for (i = 0; i < n; i++)
-	{
 		for (j = 0; j < m; j++)
-		{
 			scanf("%d", &max[i][j]);
-		}
-	}
 	printf("Enter the available matrix: \n");
 
 	for (i = 0; i < m; i++)
-	{
 		scanf("%d", &available[i]);
-	}
 	for (i = 0; i < n; i++)
-	{
 		for (j = 0; j < m; j++)
-		{
 			need[i][j] = max[i][j] - allocation[i][j];
-		}
-	}
-
 	p = 1;
 	y = 0;
 	while (p != 0)
@@ -46,27 +31,19 @@ int main()
 		{
 			z = 0;
 			for (j = 0; j < m; j++)
-			{
 				if (need[i][j] <= available[j] && (need[i][0] != -1))
-				{
 					z++;
-				}
-			}
 			if (z == n)
 			{
 				for (k = 0; k < m; k++)
-				{
 					available[k] += allocation[i][k];
-				}
 				printf("SS process %d", i);
 				need[i][0] = -1;
 				y++;
 			}
 		}
 		if (y == n)
-		{
 			p = 0;
-		}
 	}
 
 	return 0;
---
+++ sem 4/Shell programs/add_two_numbers.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Enter the first number: "
+read num1
+echo "Enter the second number: "
+read num2
+echo "Result = $((num1 + num2))"
