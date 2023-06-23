331cb1a9d59b4228d6fc40f82bc09a0ae66b6da1
Akash R Chandran <chandranrakash@gmail.com>
2023-06-23 23:39:55 +0530

    updated leaky bucket program

--- sem 6/Networking Lab/Leaky Bucket/program.c
+++ sem 6/Networking Lab/Leaky Bucket/program.c
@@ -1,28 +1,62 @@
 #include <stdio.h>
-int main()
+#include <stdlib.h>
+struct packet
 {
-    int incoming, outgoing, buck_size, n, store = 0;
-    printf("Enter bucket size, outgoing rate and no of inputs: ");
-    scanf("%d %d %d", &buck_size, &outgoing, &n);
-
-    while (n != 0)
+    int time;
+    int size;
+} P[50];
+void main()
+{
+    int i, n, m, k = 0;
+    int bsize, bfilled, outrate;
+    printf("Number of packets:");
+    scanf("%d", &n);
+    printf("Enter the packets in the order of their arrival time:\n");
+    for (i = 0; i < n; i++)
     {
-        printf("Enter the incoming packet size : ");
-        scanf("%d", &incoming);
-        printf("Incoming packet size %d\n", incoming);
-        if (incoming <= (buck_size - store))
+        printf("Enter the time and size:");
+        scanf("%d%d", &P[i].time, &P[i].size);
+    }
+    printf("Enter the bucket size: ");
+    scanf("%d", &bsize);
+    printf("Enter the output rate: ");
+    scanf("%d", &outrate);
+    m = P[n - 1].time;
+    i = 1;
+    k = 0;
+    bfilled = 0;
+    while (i <= m || bfilled != 0)
+    {
+        printf("\n\nAt time %d", i);
+        if (P[k].time == i)
         {
-            store += incoming;
-            printf("Bucket buffer size %d out of %d\n", store, buck_size);
+            if (bsize >= bfilled + P[k].size)
+            {
+                bfilled = bfilled + P[k].size;
+                printf("\n%d byte packet is inserted", P[k].size);
+                k = k + 1;
+            }
+            else
+            {
+                printf("\n%d byte packet is discarded", P[k].size);
+                k = k + 1;
+            }
+        }
+        if (bfilled == 0)
+        {
+            printf("\n no packets to transmit");
+        }
+        else if (bfilled >= outrate)
+        {
+            bfilled = bfilled - outrate;
+            printf("\n %d byte tranformed", outrate);
         }
         else
         {
-            printf("Dropped %d no of packets\n", incoming - (buck_size - store));
-            printf("Bucket buffer size %d out of %d\n", store, buck_size);
-            store = buck_size;
+            printf("\n%d bytes transfered", bfilled);
+            bfilled = 0;
         }
-        store = store - outgoing;
-        printf("After outgoing %d packets left out of %d in buffer\n", store, buck_size);
-        n--;
+        printf("\npackets in the bucket %d byte\n", bfilled);
+        i++;
     }
 }
